a7b3bbb27ad1b3943d78b323eb63ce6eda208bbd
Luris Solis <luris.solis@students.snow.edu>
2024-02-27 12:59:31 -0700

    add the controllers and the some services

---
+++ Bakery.WebApp/Controllers/ItemPurchaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bakery.WebApp.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemPurchaseController : ControllerBase
+    {
+    }
+}
