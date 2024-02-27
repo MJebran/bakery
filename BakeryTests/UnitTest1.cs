2c4f265f8ff5caca0d3c65ecd74f20c0d4628115
Ahmad Jebran <ahmad.jebran@students.snow.edu>
2024-02-26 17:14:09 -0700

    added <OutputType Condition="'$(TargetFramework)' != 'net8.0'">Exe</OutputType> to MAUI csproj and also added "net8.0;" in de begning of file to fix the errors when I added the test projects

--- BakeryTests/UnitTest1.cs
+++ BakeryTests/UnitTest1.cs
@@ -2,10 +2,10 @@ namespace BakeryTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        //[Fact]
+        //public void Test1()
+        //{
 
-        }
+        //}
     }
 }
