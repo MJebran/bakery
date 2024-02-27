b184665831ee63396af261bf23f67d301ee30c31
Ahmad Jebran <ahmad.jebran@students.snow.edu>
2024-02-26 18:06:45 -0700

    added local db but it is missing out tables, we need to work on that, I did not add them because I wanted you guys to be involved

---
+++ Bakery.Mobile/Inits/LocalDbInit.cs
@@ -0,0 +1,51 @@
+﻿using Bakery.ClassLibrary.Services;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery.Mobile.Inits;
+
+public class LocalDbInit(IDbLocation dbLocation)
+{
+    // TODO
+    //private Type[] tables = [typeof(), typeof()] //// we put tabels in side typeof(), as many as we have. 
+
+    public async Task<SQLiteAsyncConnection> InitializeLocalDatabase()
+    {
+        var database = EstablishConnection();
+
+        await database.CreateTablesAsync(CreateFlags.None/*, tables*/);
+
+        return database;
+    }
+
+    public SQLiteAsyncConnection EstablishConnection()
+    {
+        if(!Directory.Exists(dbLocation.BaseDataDirecory)) Directory.CreateDirectory(dbLocation.BaseDataDirecory);
+
+        var database = new SQLiteAsyncConnection(Path.Combine(dbLocation.BaseDataDirecory, dbLocation.DbName));
+
+        return database;
+    }
+
+    public async Task EmptyDatabase()
+    {
+        var db = EstablishConnection();
+
+        // TODO we need to give our table name in the <> parts, we need to give all the tables that we have.
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+        //await db.DeleteAllAsync<>();
+    }
+
+}
---
+++ Bakery.Mobile/Services/MauiDbLocation.cs
@@ -0,0 +1,14 @@
+﻿using Bakery.ClassLibrary.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bakery.Mobile.Services;
+
+public class MauiDbLocation : IDbLocation
+{
+    public string DbName { get => "Bakery.db"; }
+    public string BaseDataDirecory { get => FileSystem.Current.AppDataDirectory; }
+}
