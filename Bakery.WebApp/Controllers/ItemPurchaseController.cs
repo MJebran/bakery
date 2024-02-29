1428b75edfca2a6c817373075ed4ae1cff7f2bf7
MJebran <jebran.mustafa@gmail.com>
2024-02-29 12:40:13 -0700

    Mustafa Did the API for ItemPurchase, Luris un commnet the program cs for ItemPurchaseService part

--- Bakery.WebApp/Controllers/ItemPurchaseController.cs
+++ Bakery.WebApp/Controllers/ItemPurchaseController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Bakery.WebApp.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bakery.WebApp.Services
@@ -7,5 +8,35 @@ namespace Bakery.WebApp.Services
     [ApiController]
     public class ItemPurchaseController : ControllerBase
     {
+        ItemPurchaseService _service;
+
+        public ItemPurchaseController(ItemPurchaseService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("itempurchases")]
+        public async Task<IEnumerable<Itempurchase>> GetAllItemPurchasesAsync()
+        {
+            return await _service.GetAllItempurchase();
+        }
+
+        [HttpGet("itempurchase/{id}")]
+        public async Task<Itempurchase> GetItemPurchaseByIdAsync(int id)
+        {
+            return await _service.GetItempurchaseById(id);
+        }
+
+        [HttpPost("add/itempurchase")]
+        public async Task AddItemPurchaseAsync(Itempurchase itempurchase)
+        {
+            await _service.AddItempurchase(itempurchase);
+        }
+
+        [HttpDelete("delete/itempurchase/{id}")]
+        public async Task DeleteItemPurchaseAsync(int id)
+        {
+            await _service.DeleteItempurchase(id);
+        }
     }
 }
---
+++ Bakery.WebApp/Services/ItemPurchaseService.cs
@@ -0,0 +1,54 @@
+﻿using Bakery.ClassLibrary.Services;
+using Bakery.WebApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Bakery.WebApp.Services
+{
+    public class ItemPurchaseService(IDbContextFactory<PostgresContext> dbFactory) : IItemPurchaseService
+    {
+        public async Task AddItempurchase(Itempurchase size)
+        {
+            var context = await dbFactory.CreateDbContextAsync();
+            context.Itempurchases.Add(size);
+            context.SaveChanges();
+        }
+
+        public async Task DeleteItempurchase(int id)
+        {
+            var context = await dbFactory.CreateDbContextAsync();
+            var itempurchase = new Itempurchase()
+            {
+                ItempurchaseId = id
+            };
+            context.Itempurchases.Remove(itempurchase);
+            context.SaveChanges();
+        }
+
+        public async Task<IEnumerable<Itempurchase>> GetAllItempurchase()
+        {
+            var context = await dbFactory.CreateDbContextAsync();
+            var itempurchases = await context.Itempurchases.ToListAsync();
+            return itempurchases;
+        }
+
+        public async Task<Itempurchase> GetItempurchaseById(int id)
+        {
+            var context = await dbFactory.CreateDbContextAsync();
+            var itempurchase = await context.Itempurchases.Where(item => item.ItempurchaseId == id).FirstOrDefaultAsync();
+            return itempurchase;  
+        }
+
+        public async Task UpdateItempurchase(int id)
+        {
+            var context = await dbFactory.CreateDbContextAsync();
+            var itempurchase = new Itempurchase()
+            {
+                ItempurchaseId = id
+            };
+            context.Itempurchases.Update(itempurchase);
+            context.SaveChanges();
+        }
+
+    }
+}
