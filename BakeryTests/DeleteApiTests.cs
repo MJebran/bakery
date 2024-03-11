1e5b50cdb28f90c7879fe23d5cf96119a154d510
MJebran <jebran.mustafa@gmail.com>
2024-03-11 16:31:40 -0600

    added DeleteItemTypesTest

--- BakeryTests/DeleteApiTests.cs
+++ BakeryTests/DeleteApiTests.cs
@@ -32,13 +32,61 @@ namespace BakeryTests
             var requestContent = new HttpRequestMessage(HttpMethod.Delete, "api/user/delete/user");
             requestContent.Content = new StringContent(JsonConvert.SerializeObject(userSerialized), Encoding.UTF8, "application/json");
             await this.client.SendAsync(requestContent);
-            //var requestContent = new StringContent(userSerialized, Encoding.UTF8, "application/json");
-            //var x = await client.DeleteAsync("api/user/delete/user", requestContent);
             var getcategories = await client.GetFromJsonAsync<List<User>>("api/user/users");
 
             //Assert
             getcategories.Should().BeEmpty();
         }
+
+        [Fact]
+        public async void DeleteItemTypeTest()
+        {
+            //Arrange
+            var product = new Itemtype()
+            {
+                ItemTypeId = 1,
+                ItemName = "Test1",
+                ItemPrice = 1,
+                ItmeCalories = 1,
+                ItemWeight = 1,
+                ItemDescription = "Test1",
+                CategoryId = 1,
+                SizeId = 1
+
+            };
+            //Act itemtype/add/itemtype
+            await client.PostAsJsonAsync("api/itemtype/add/itemtype", product);
+            var productSerialized = System.Text.Json.JsonSerializer.Serialize(product);
+            var requestContent = new HttpRequestMessage(HttpMethod.Delete, "api/itemtype/delete/itemtype");
+            requestContent.Content = new StringContent(JsonConvert.SerializeObject(productSerialized), Encoding.UTF8, "application/json");
+            await this.client.SendAsync(requestContent);
+            var getcategories = await client.GetFromJsonAsync<List<Itemtype>>("api/itemtype/itemtypes");
+
+            //Assert
+            getcategories.Should().BeEmpty();
+        }
+
+        //[Fact]
+        //public async void DeleteCategoryTest()
+        //{
+        //    //Arrange
+        //    var category = new Category()
+        //    {
+        //        CategoryId = 1,
+        //        CategoryName = "Test1",
+        //        CategoryDescription = "Test1"
+        //    };
+        //    //Act 
+        //    await client.PostAsJsonAsync("api/category/add/category", category);
+        //    var categorySerialized = System.Text.Json.JsonSerializer.Serialize(category);
+        //    var requestContent = new HttpRequestMessage(HttpMethod.Delete, "api/category/delete/category");
+        //    requestContent.Content = new StringContent(JsonConvert.SerializeObject(categorySerialized), Encoding.UTF8, "application/json");
+        //    await this.client.SendAsync(requestContent);
+        //    var getcategories = await client.GetFromJsonAsync<List<Category>>("api/category/categories");
+
+        //    //Assert
+        //    getcategories.Should().BeEmpty();
+        //}
 
 
 
