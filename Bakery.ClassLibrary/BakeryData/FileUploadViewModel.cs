bca4d027c7102e4fce64e6df2f7764aeca118b12
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2024-03-29 11:57:41 -0600

    Fixed errors

---
+++ Bakery.ClassLibrary/BakeryData/FileUploadViewModel.cs
@@ -0,0 +1,8 @@
+namespace Bakery.WebApp.Data;
+public class FileUploadViewModel
+{
+    public string? FileName { get; set; }
+    public string? FileStorageUrl { get; set; }
+    public string? ContentType { get; set; }
+}
+
