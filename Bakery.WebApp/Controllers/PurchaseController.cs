f09a33cde65352341a2a8f3cabc80a3d68ffc4e2
MJebran <jebran.mustafa@gmail.com>
2024-04-12 15:44:25 -0600

    To fix the circular dependency we did Maping with Johnathon. this made the API very fast and it mapd it without doing alot of change. We do need to create the DTOS  and Mapper class.

--- Bakery.WebApp/Controllers/PurchaseController.cs
+++ Bakery.WebApp/Controllers/PurchaseController.cs
@@ -1,51 +1,115 @@
 ﻿using Bakery.ClassLibrary.Services;
+using Bakery.WebApp.Components.Pages;
 using Bakery.WebApp.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Riok.Mapperly.Abstractions;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
 
-namespace Bakery.WebApp.Services
+namespace Bakery.WebApp.Services;
+
+public class PurchaseDTO 
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PurchaseController : ControllerBase
+   
+    public int PurchaseId { get; set; }
+
+    public DateOnly? PurcahseDate { get; set; }
+
+    public bool Ispayed { get; set; }
+
+    
+    public virtual  IEnumerable<ItempurchaseDTO> Itempurchases { get; set; }
+
+}
+
+public class ItempurchaseDTO
+{
+    public int ItempurchaseId { get; set; }
+    public int? ItempurchaseQuantity { get; set; }
+    public virtual CustomItemDTO ItempurchaseItem { get; set; } = null!;
+}
+
+public class CustomItemDTO
+{
+    public int CustomItemId { get; set; }
+    public virtual ICollection<CustomitemtoppingDTO> Customitemtoppings { get; set; }
+}
+
+
+
+public class CustomitemtoppingDTO
+{
+    public int CustomItemToppingId { get; set; }
+
+    public int? CustomItemToppingQuantity { get; set; }
+
+    //[ForeignKey(typeof(Topping))]
+    //public int ToppingId { get; set; }
+
+    //[ManyToOne]
+    //public virtual Customitem CustomItem { get; set; } = null!;
+
+    //[ManyToOne]
+    //public virtual Topping Topping { get; set; } = null!;
+}
+
+
+[Mapper]
+public partial class BakeryMapper
+{
+    public partial PurchaseDTO PurchaseToPurchaseDto(Purchase purchase);
+}
+
+
+
+
+
+
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class PurchaseController : ControllerBase
+{
+    IPurchaseService _service;
+    BakeryMapper _mapper = new BakeryMapper();
+    public PurchaseController(IPurchaseService service)
     {
-        IPurchaseService _service;
-        public PurchaseController(IPurchaseService service)
-        {
-            _service = service;
-        }
+        _service = service;
+    }
 
-        [HttpGet("purchases")]
+    [HttpGet("purchases")]
 
-        public async Task<IEnumerable<Purchase>> GetAllPurchases()
-        {
-            return await _service.GetAllPurchase();
-        }
+    public async Task<IEnumerable<PurchaseDTO>> GetAllPurchases()
+    {
 
-        [HttpGet("purchase/{id}")]
+        return (await _service.GetAllPurchase()).Select(p => _mapper.PurchaseToPurchaseDto(p));  // used the _mapper in here
+    }
 
-        public async Task<Purchase> GetPurchaseAsync(int id)
-        {
-            return await _service.GetPurchase(id);
-        }
+    [HttpGet("purchase/{id}")]
 
-        [HttpPost("add/purchase")]
+    public async Task<Purchase> GetPurchaseAsync(int id)
+    {
+        return await _service.GetPurchase(id);
+    }
 
-        public async Task AddPurchaseAsycn([FromBody] Purchase purchase)
-        {
-            await _service.AddPurchase(purchase);
-        }
+    [HttpPost("add/purchase")]
 
-        [HttpDelete("delete/purchase/{id}")]
-        public async Task DeletePurchaseAsync(int id)
-        {
-            await _service.DeletePurchase(id);
-        }
+    public async Task AddPurchaseAsycn([FromBody] Purchase purchase)
+    {
+        await _service.AddPurchase(purchase);
+    }
 
-        [HttpPut("update/purchase/{id}")]
-        public async Task UpdatePurchaseAsync(int id)
-        {
-            await _service.UpdatePurchase(id);
-        }
+    [HttpDelete("delete/purchase/{id}")]
+    public async Task DeletePurchaseAsync(int id)
+    {
+        await _service.DeletePurchase(id);
+    }
+
+    [HttpPut("update/purchase/{id}")]
+    public async Task UpdatePurchaseAsync(int id)
+    {
+        await _service.UpdatePurchase(id);
     }
 }
