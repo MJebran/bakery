a0f7b01cbd7d4a895f3028f669de2dc92207d2b4
MJebran <jebran.mustafa@gmail.com>
2024-04-17 15:38:51 -0600

    Trying to fix the css that dose not dispaly

---
+++ Bakery.ClassLibrary/wwwroot/js/Checkout.js
@@ -0,0 +1,45 @@
+export function initialisePayPal() {
+    var description = document.querySelector('#smart-button-container #description');
+    var amount = document.querySelector('#smart-button-container #amount');
+
+    var purchase_units = [];
+    purchase_units[0] = {};
+    purchase_units[0].amount = {};
+
+    paypal.Buttons({
+        style: {
+            color: 'gold',
+            shape: 'rect',
+            label: 'paypal',
+            layout: 'vertical',
+
+        },
+
+        createOrder: function (data, actions) {
+
+            purchase_units[0].description = description.value;
+            purchase_units[0].amount.value = amount.value;
+
+            return actions.order.create({
+                purchase_units: purchase_units,
+            });
+        },
+
+        onApprove: function (data, actions) {
+            return actions.order.capture().then(function (orderData) {
+
+                console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+                window.location.href = "/thankYou";
+            });
+        },
+
+        onError: function (err) {
+            console.log(err);
+        }
+    }).render('#paypal-button-container');
+}
+
+export function onUpdate() {
+    initialisePayPal();
+}
+
