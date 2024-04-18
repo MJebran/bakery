76c61dbdabf6cea283856bf297da8765601a4ae2
luris.solis <luris.solis@students.snoe.edu>
2024-04-18 14:32:17 -0600

    Azure Map works

---
+++ Bakery.WebApp/wwwroot/map.js
@@ -0,0 +1,36 @@
+﻿var map;
+
+function initializeMap(mapsKey) {
+    // Initialize the Azure Maps
+    atlas.setSubscriptionKey(mapsKey);
+    // Create the map instance
+    map = new atlas.Map("mapDiv", {
+        view: "Auto",
+        center: [-122.129, 47.640],
+        zoom: 3
+    });
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var userLocation = [position.coords.longitude, position.coords.latitude];
+        map.setCamera({
+            center: userLocation,
+            zoom: 13
+        });
+    });
+
+    var marker = new atlas.HtmlMarker({
+        color: 'DodgerBlue',
+        text: 'O',
+        position: [- 111.583795, 39.360108],
+        popup: new atlas.Popup({
+            content: `<div style="padding:10px">You Are Here =)</div>`,
+            pixelOffset: [0, -30]
+        })
+    });
+    map.markers.add(marker);
+
+    map.events.add('click', marker, () => {
+        marker.togglePopup();
+    });
+}
+
